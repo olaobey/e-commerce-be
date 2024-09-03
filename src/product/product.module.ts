2ff57f09a37c53799ad9fcb03bd1bd61a40088ff
olaobey <olaobey15@gmail.com>
2024-09-03 08:26:49 +0100

    first commit

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { Product } from '../entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService, JwtAuthGuard],
 })
 export class ProductModule {}
